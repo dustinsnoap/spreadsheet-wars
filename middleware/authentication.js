a26710bc438a96b55d0bda06bfb05793aa1e0d30
dustinsnoap <dustinsnoap@gmail.com>
2020-12-20 00:26:31 -0500

    middleware -finished check_username

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,10 +6,11 @@ const TABLE = 'users'
 
 //TEMP move to more global position
 class error {
-    constructor(type, field=null, code=null) {
+    constructor({type, field=null, code=null, detail=null}) {
         this.type = type
         this.field = field
         this.code = code
+        this.detail = detail
     }
 }
 
@@ -18,20 +19,26 @@ const check_username = async (req, res, next) => {
     req.errors = []
 
     //check if there is a username field
-    if(!req.body.username)
-        req.errors.push(new error('missing field', 'username'))
+    if(!req.body.username) {
+        req.errors.push(new error({type:'missing field', field:'username'}))
+        next(); return
+    }
 
-    //clean up username
-        //regex to make it only alphanumeric
-    
-    //check if username fits criteria
-        //add a settings config to configure this
+    //CHECK IF CRITERIA IS MET
+        //clean up username
+        if(req.body.username !== req.body.username.replace(/\W/g, ''))
+            req.errors.push(new error({type:'invalid characters', field:'username', detail:'Only alphanumeric allowed.'}))
+
+        //check username length
+        if(req.body.username.length < 3 || req.body.username.length > 16)
+            req.errors.push(new error({type:'field length', field:'username', detail:'Username must be between 3 and 16 characters.'}))
+
+        //skip if criteria isn't met
+        if(req.errors.length) {next(); return}
     
     //check if username is unique
-    const test = await get_one(TABLE, {username: 'farts'})
-    console.log('test', test)
-    if(await get_one(TABLE, {username: 'farts'}))
-        req.errors.push(new error('duplicate', 'username'))
+    if(await get_one(TABLE, {username: req.body.username}))
+        req.errors.push(new error({type:'duplicate', field:'username', detail:'Username already exists.'}))
 
     next()
 }
