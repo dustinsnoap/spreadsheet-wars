56d05444f6caf35f079ed3e11c260d96cf29cc7d
dustinsnoap <dustinsnoap@gmail.com>
2020-12-20 04:13:52 -0500

    middleware: password_criteria checks for password length hashes password if no authentication errors

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -19,6 +19,26 @@ class error {
 }
 
 const password_criteria = (req, res, next) => {
+    //initialize error variable if not already
+    req.errors = req.errors ? req.errors : []
+
+    //check if there is a password field
+    if(!req.body.password) {
+        req.errors.push(new error({type: 'missing field', field: 'password'}))
+        next(); return
+    }
+
+    //CHECK PASSWORD CRITERIA
+        //check password length
+        if(req.body.password.length < settings.password_length)
+            req.errors.push(new error({
+                type: 'field length',
+                field: 'password',
+                detail: `Password must be longer than ${settings.password_length} characters.`}))
+    
+    //HASH PASSWORD
+    if(!req.errors.length) //only hash password if it's going to be used
+        req.body.password = crypt.hashSync(req.body.password, settings.password_difficulty)
 
     next()
 }
@@ -29,25 +49,36 @@ const username_criteria = async (req, res, next) => {
 
     //check if there is a username field
     if(!req.body.username) {
-        req.errors.push(new error({type:'missing field', field:'username'}))
+        req.errors.push(new error({
+            type: 'missing field',
+            field: 'username'}))
         next(); return
     }
 
     //CHECK IF CRITERIA IS MET
         //clean up username
         if(req.body.username !== req.body.username.replace(/\W/g, ''))
-            req.errors.push(new error({type:'invalid characters', field:'username', detail:'Only alphanumeric allowed.'}))
+            req.errors.push(new error({
+                type:' invalid characters',
+                field:' username',
+                detail:' Only alphanumeric allowed.'}))
 
         //check username length
         if(req.body.username.length < settings.username_min_length || req.body.username.length > settings.username_max_length)
-            req.errors.push(new error({type:'field length', field:'username', detail:'Username must be between 3 and 16 characters.'}))
+            req.errors.push(new error({
+                type: 'field length',
+                field: 'username',
+                detail: `Username must be between ${settings.username_min_length} and ${settings.username_max_length} characters.`}))
 
         //skip if criteria isn't met
         if(req.errors.length) {next(); return}
     
     //check if username is unique
     if(await get_one(TABLE, {username: req.body.username}))
-        req.errors.push(new error({type:'duplicate', field:'username', detail:'Username already exists.'}))
+        req.errors.push(new error({
+            type: 'duplicate',
+            field: 'username',
+            detail: 'Username already exists.'}))
 
     next()
 }
